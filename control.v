68ba26841b02025f4ffb18a87c7d3f189ffc3150
Rishabh Jain <f2015232@goa.bits-pilani.ac.in>
2018-04-22 17:50:11 +0530

    Delete control.v

--- control.v
+++
@@ -1,7 +0,0 @@
-module control(opcode,Sel1, PCsrc,RegWrite     );
-input [1:0] opcode;
-output Sel1, PCsrc, RegWrite;
-assign Sel1 = (opcode == 2'b00)? 1:0;
-assign PCsrc = (opcode == 2'b11)? 1: 0;
-assign RegWrite = (opcode ==2'b11)? 1:0 ;
-endmodule
